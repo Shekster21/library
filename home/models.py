5d2ff3d3467ee019e35c29faaba1e4e3ed4188c4
shekster21 <p.abhi.p07@gmail.com>
2018-09-18 21:23:44 +0530

    Third commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Catalogue(models.Model):
+    BookNumber = models.IntegerField(unique=True)
+    Title = models.CharField(max_length = 100)
+    Author = models.CharField(max_length = 100)
+    Category = models.CharField(max_length = 50)
+    Pages = models.IntegerField()
+    Publisher = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.Title
+
+
